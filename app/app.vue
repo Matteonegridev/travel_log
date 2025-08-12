e54e4e305214bc6c690a590a946a5f0c6dcd9af8
Matteo <matteonegridev@gmail.com>
2025-08-12 11:05:39 +0200

    formatted css

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="mt-6 flex">
+      <button class="btn btn-primary">
+        <span>
+          <Icon
+            name="tabler:location"
+            style="color: bisque;"
+          />
+        </span>  Primary
+      </button>
+    </div>
   </div>
 </template>
