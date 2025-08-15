7a96ffb09b574455631b4b3c196e6df072949b9c
Matteo <matteonegridev@gmail.com>
2025-08-15 17:52:13 +0200

    removed ! from index.ts in database

--- server/database/index.ts
+++ server/database/index.ts
@@ -7,9 +7,9 @@ import * as schema from "./schema";
 // You can specify any property from the libsql connection options
 const db = drizzle({
   connection: {
-    url: env.TURSO_DATABASE_URL!,
+    url: env.TURSO_DATABASE_URL,
     // run whn not in development:
-    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN!,
+    authToken: env.NODE_ENV === "development" ? undefined : env.TURSO_AUTH_TOKEN,
   },
   casing: "snake_case",
   schema,
